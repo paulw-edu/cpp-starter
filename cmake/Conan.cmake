e3aa2a7e479e6b980bede983d958a97dbfae9cec
Paul Walker <paul.walker@foerstergroup.com>
2021-05-06 09:33:29 +0200

    added gtest

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -16,9 +16,7 @@ macro(run_conan)
   conan_cmake_run(
     REQUIRES
     ${CONAN_EXTRA_REQUIRES}
-    catch2/2.13.3
-    docopt.cpp/0.6.2
-    fmt/6.2.1
+    gtest/1.10.0
     spdlog/1.5.0
     OPTIONS
     ${CONAN_EXTRA_OPTIONS}
